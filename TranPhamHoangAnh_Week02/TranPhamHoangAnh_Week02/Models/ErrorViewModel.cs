e8a7635f039f39c1e67f0cbdf6ec1efb96442d3d
Hoang Anh <288hoanganh@gmail.com>
2025-05-11 00:44:05 +0700

    Create lap week 02

---
+++ TranPhamHoangAnh_Week02/TranPhamHoangAnh_Week02/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TranPhamHoangAnh_Week02.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
